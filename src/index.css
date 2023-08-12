e8d1a7f77e2d94b4de3172efba156e875f1e9b88
Mustafa <mostafakhaled0787314@gmail.com>
2023-08-12 17:51:34 +0300

    overlay Changes

--- src/index.css
+++ src/index.css
@@ -119,7 +119,6 @@ html {
 body {
   font-family: "Poppins", sans-serif;
   color: var(--color-grey-700);
-
   transition: color 0.3s, background-color 0.3s;
   min-height: 100vh;
   line-height: 1.5;
