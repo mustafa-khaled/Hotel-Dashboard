75501b36d809cb16ebe82c1d7db5118f3b77c3ee
Mustafa <mostafakhaled0787314@gmail.com>
2023-08-21 22:33:22 +0300

    error handling for create guest form

--- src/features/guests/CreateGuestForm.jsx
+++ src/features/guests/CreateGuestForm.jsx
@@ -7,7 +7,7 @@ import Button from "../../ui/button/Button";
 import SpinnerMini from "../../ui/spinnerMini/SpinnerMini";
 
 function CreateGuestForm({ onCloseModal }) {
-  const { register, handleSubmit, reset } = useForm();
+  const { register, handleSubmit, reset, formState } = useForm();
   const { createGuest, isCreating } = useCreateGuest();
 
   function onSubmit(data) {
@@ -22,11 +22,13 @@ function CreateGuestForm({ onCloseModal }) {
     );
   }
 
+  const { errors } = formState;
+
   return (
     <Form
       submit={handleSubmit(onSubmit)}
       type={onCloseModal ? "modal" : "regular"}>
-      <FormRow label="Full Name">
+      <FormRow label="Full Name" error={errors?.fullName?.message}>
         <input
           id="FullName"
           type="text"
@@ -36,17 +38,23 @@ function CreateGuestForm({ onCloseModal }) {
         />
       </FormRow>
 
-      <FormRow label="Email">
+      <FormRow label="Email" error={errors?.email?.message}>
         <input
           id="email"
           type="text"
           className="form-input"
           disabled={isCreating}
-          {...register("email", { required: "This Field is required" })}
+          {...register("email", {
+            required: "This Field is required",
+            pattern: {
+              value: /\S+@\S+\.\S+/,
+              message: "Please Provide A valid email address",
+            },
+          })}
         />
       </FormRow>
 
-      <FormRow label="National Id">
+      <FormRow label="National Id" error={errors?.nationalID?.message}>
         <input
           id="nationalId"
           type="text"
@@ -56,7 +64,7 @@ function CreateGuestForm({ onCloseModal }) {
         />
       </FormRow>
 
-      <FormRow label="Nationality">
+      <FormRow label="Nationality" error={errors?.nationality?.message}>
         <input
           id="nationality"
           type="text"
@@ -84,7 +92,9 @@ function CreateGuestForm({ onCloseModal }) {
           onClick={() => onCloseModal?.()}>
           Cancel
         </Button>
-        <Button disabled={isCreating}>
+        <Button
+          disabled={isCreating}
+          variation={isCreating ? "secondary" : "primary"}>
           {isCreating ? <SpinnerMini /> : "Create New Guest"}
         </Button>
       </FormRow>
